67f9897db8a6e65885ff62a4610e2be689b1b6f4
Spit-ball <83725076+Spit-ball@users.noreply.github.com>
2021-06-30 18:43:59 -0500

    Version 0.0.1

---
+++ main.py
@@ -0,0 +1,42 @@
+import nltk_testing
+from nltk_testing import languageProcessing
+
+# Story templates store the main story data
+
+storyTemplate_1 = str("a")
+
+storyTemplate_2 = str("b")
+
+storyTemplate_3 = str("c")
+
+# Noun inputs allow user to input nouns and then stores input for later use
+
+noun_1 = input("Please enter a proper noun: ")
+
+noun_2 = input("Please enter a proper noun: ")
+
+noun_3 = input("Please enter a proper noun: ")
+
+noun_4 = input("Please enter a proper noun: ")
+
+noun_5 = input("Please enter a proper noun: ")
+
+verb_1 = input("Please enter a verb: ")
+
+verb_2 = input("Please enter a verb: ")
+
+verb_3 = input("Please enter a verb: ")
+
+verb_4 = input("Please enter a verb: ")
+
+verb_5 = input("Please enter a verb: ")
+
+nltk_testing.languageProcessing(noun_1)
+
+nltk_testing.languageProcessing(noun_2)
+
+nltk_testing.languageProcessing(noun_3)
+
+nltk_testing.languageProcessing(noun_4)
+
+nltk_testing.languageProcessing(noun_5)
